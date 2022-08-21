209fccbd3cd135d118cf2281b82fc3fe279f9c3f
islom <islom@islom>
2022-08-21 17:49:48 +0500

    make it better

--- next.config.js
+++ next.config.js
@@ -7,18 +7,18 @@ const nextConfig = {
 		APP_ENV: process.env.REACT_APP_ENV,
 		APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
 	},
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: `https://magic-tv-back.herokuapp.com/:path*`,
-			},
-			{
-				source: '/uploads/:path*',
-				destination: `https://magic-tv-back.herokuapp.com/uploads/:path*`,
-			},
-		]
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: '/api/:path*',
+	// 			destination: `https://magic-tv-back.herokuapp.com/:path*`,
+	// 		},
+	// 		{
+	// 			source: '/uploads/:path*',
+	// 			destination: `https://magic-tv-back.herokuapp.com/uploads/:path*`,
+	// 		},
+	// 	]
+	// },
 }
 
 module.exports = nextConfig
